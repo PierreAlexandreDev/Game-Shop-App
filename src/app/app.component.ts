0cbf40bc86276b3b6543122ce218f5028d0a8f7b
PierreAlexandreDev <pierrealexstyle@gmail.com>
2020-05-14 10:19:10 +0200

    cart counter: increment and decrement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'game-shop-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
   name = 'Pierre-Alexandre';
+  appname = 'game-shop-app';
   counter = 1;
+
+  increment_counter(){
+    this.counter = this.counter + 1;
+  }
+
+  decrement_counter(){
+    this.counter = this.counter - 1;
+  }
 }
